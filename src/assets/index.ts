64cafc5415beab8b269271849b5e727fbcb39591
Ariyan Rahman Anas <anas.hllw@gmail.com>
2026-04-06 16:55:28 +0600

    removed commented code and modified hero

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,10 @@
 import Anas from "./../../public/Anas.png"
 import BAMHS from "./../../public/bamhs.png"
+import BAMHS2 from "./../../public/bamhs2.png"
 
 
 export const staticImages = {
     Anas,
-    bamhsBackground: BAMHS,
+    bamhsBackground1: BAMHS,
+    bamhsBackground: BAMHS2,
 }
